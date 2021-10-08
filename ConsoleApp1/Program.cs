bb0ada1ce66326fa3015b6b77b729ca93e1856f0
David Gordon <44374315+tehtko@users.noreply.github.com>
2021-10-08 10:54:18 -0400

    Add comments

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace ConsoleApp1
 {
@@ -7,11 +7,17 @@ namespace ConsoleApp1
     {
         public static void Main(string[] args)
         {
+
+            //How many digits in the sequence do you want to see?
+            Console.WriteLine("Iterations;");
             string iterations;
             int i = Convert.ToInt32(iterations = Console.ReadLine());
-            Int64 num1 = 1, num2 = 1, temp;
+
+            //Declare variables
+            Int64 num1 = 0, num2 = 1, temp;
             for(int x = 0; x != i; x++)
             {
+                //Store temp as first number, it will always be the previous number going forward
                 temp = num2;
                 num2 = num1 + num2;
                 num1 = temp;
